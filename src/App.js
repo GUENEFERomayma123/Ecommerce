699c2b28b423a6849811849f644262ee60535b45
Omayma GUENEFER <guenefero@gmail.com>
2022-12-17 20:49:26 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Footer } from "./components/footer/Footer"
+import { Header } from "./components/header/Header"
+import { Home } from "./pages/home/Home"
+import { Login } from "./pages/login/Login"
+import { useSelector } from "react-redux"
+
+const App = () => {
+  const isLoggIn = useSelector((state) => state.auth.isLoggIn)
+  const cartItems = useSelector((state) => state.cart.itemsList)
+  console.log(cartItems)
+  return (
+    <>
+      {isLoggIn && (
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+          <Footer />
+        </Router>
+      )}
+      {!isLoggIn && <Login />}
+    </>
+  )
+}
+export default App
